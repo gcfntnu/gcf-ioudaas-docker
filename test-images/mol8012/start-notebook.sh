15c83bad3906a5f97cf2394f73cdb3db653895e0
Geir A <geiramh@stud.ntnu.no>
2019-09-18 16:24:32 +0200

    added test images

---
+++ test-images/mol8012/start-notebook.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# Exec the specified command or fall back on bash
+if [ $# -eq 0 ]; then
+    cmd=bash
+else
+    cmd=$*
+fi
+
+if [ ! -d "$HOME/.jupyter" ]; then
+	cp -r /opt/.jupyter $HOME/.jupyter
+fi
+
+if [ ! -f "$HOME/.jupyter/notebook_config.py" ]; then
+	cp -r /opt/.jupyter/notebook_config.py $HOME/.jupyter
+fi
+
+# If we have shared data mounted, the link it to current directory to have it visible in notebook
+if [ -d "/mnt/data" ]; then
+	rm -f $HOME/data
+	ln -sf /mnt/data $HOME/data
+fi
+
+
+if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+	jupyter-labhub --config "$HOME/.jupyter/notebook_config.py"
+else
+	jupyterhub-singleuser --config "$HOME/.jupyter/notebook_config.py"
+fi
